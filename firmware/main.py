cba50ca28ffbba4d0c50c3dc63e316375cec3eaf
Dhruva Shaw <me@dhruvashaw.me>
2023-07-15 12:26:06 +0530

    Added the new libraries

---
+++ firmware/main.py
@@ -0,0 +1,13 @@
+import heapq
+from collections import deque, namedtuple
+from math import sqrt
+
+pycom.heartbeat(False)
+
+while True:
+    pycom.rgbled(0xFF0000)  # Red
+    time.sleep(1)
+    pycom.rgbled(0x00FF00)  # Green
+    time.sleep(1)
+    pycom.rgbled(0x0000FF)  # Blue
+    time.sleep(1)
